b73d9109bec8a1c569f3c1008f4274d444a22d26
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-02-07 00:02:08 +0900

    [feat] contextAPI

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
-    flex: 1;
-    padding: 20px 32px;
-    padding-bottom: 48px;
-    overflow-y: auto;
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
 `;
 
 function TodoList() {
+    const todos = useTodoState();
+
     return (
         <TodoListBlock>
-            <TodoItem text="프로젝트 생성하기" done={true} />
-            <TodoItem text="컴포넌트 스타일링" done={true} />
-            <TodoItem text="context 만들기" done={false} />
-            <TodoItem text="기능구현" done={false} />
+            {todos.map(todo => (
+                <TodoItem
+                    key={todo.id}
+                    id={todo.id}
+                    text={todo.text}
+                    done={todo.done}
+                />
+            ))}
         </TodoListBlock>
-    )
+    );
 }
 
 export default TodoList;
